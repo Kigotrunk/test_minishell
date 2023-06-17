89dc48eb98d435e51865e8f25f33ba259e306ba0
Kenzo Allegretti <kallegre@c1r7p1.42nice.fr>
2023-06-18 00:32:29 +0200

    pars ken j2_2

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: kallegre <kallegre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 17:58:04 by kortolan          #+#    #+#             */
-/*   Updated: 2023/06/15 23:23:50 by kallegre         ###   ########.fr       */
+/*   Updated: 2023/06/18 00:21:54 by kallegre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,26 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <errno.h>
 # include "ft_printf/ft_printf.h"
 # include "get_next_line/get_next_line_bonus.h"
 
 void    free_tab(char **tab);
 int     quote_check(char *str);
+int     is_ope(char c);
 int	    arg_c(char *str);
 char	*eoa_quote(char *str, char c);
 char    *eoa_str(char *str);
 int     arg_len(char *str);
 char	*get_arg(char *str);
 char	**split_args(char *str);
-void    minishell(char **argv, char **envp);
+char    **get_cmd_tab(char **argv);
+int     pipe_count(char **argv);
+char    **cmd_tab_init(int n);
+char    *ft_stradd(char *s1, char *s2);
+int     minishell(char **argv, char **envp);
 
 //is_builtin && called ft_builtins
 int     is_builtin(char *cmd);
